5de517125be72ceb7d87db3f7049127d191c3d48
ALEXIS2ES <vccamerccoaunac.edu.co>
2017-04-21 08:54:30 -0500

    firtcommit

---
+++ client/apps.js
@@ -0,0 +1,37 @@
+$(function(){
+	var l=new Crud();
+})
+
+class Crud{
+	constructor(){
+		this.submitEvent()
+	}
+
+	submitEvent(){
+		$('form').submit((event)=>{
+			event.preventDefault()
+			this.sendForm()
+		})
+	}
+
+	sendForm(){
+		alert("Capturado el Submit")
+		let form_data=new FormData();
+		form_data.append('nombre',$('#nombre').val())
+		form_data.append('edad',$('#edad').val())
+		$.ajax({
+			url: '../server/agregar',
+			dataType:'json',
+			processData: false,
+			contentType: false,
+			data: form_data,
+			type: 'POST',
+			success: function(respuesta){
+
+			},
+			error:function(){
+				alert('Error en la comuncacion con el servidor');
+			}
+		})
+	}
+}
